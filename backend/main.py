268f265572a06a2e58e0c11b89339267ac9c28c8
Vaibhav Shrivastava <innomightmail@gmail.com>
2026-01-29 12:51:43 +0530

    made routes relative

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,21 @@
+import os
 from typing import List
 from fastapi import FastAPI, UploadFile, File
-import os
 from backend.utils.face_cluster import cluster_faces
-from backend.utils.file_utils import save_grouped_photos, clear_folder
-from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
-import os
 
-UPLOAD_DIR = "backend/uploads"
-OUTPUT_DIR = "backend/output"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
+
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,18 +26,20 @@ async def upload(files: List[UploadFile] = File(...)):
     image_paths = []
 
     for file in files:
-        file_path = os.path.join("backend/uploads", file.filename)
+        file_path = os.path.join(UPLOAD_DIR, file.filename)
         with open(file_path, "wb") as f:
             f.write(await file.read())
         image_paths.append(file_path)
 
     clusters = cluster_faces(image_paths)
-    app.mount("/images", StaticFiles(directory="backend/uploads"), name="images")
 
+    app.mount("/images", StaticFiles(directory=UPLOAD_DIR), name="images")
 
-    # Convert file paths to URLs
     result = {}
     for person, imgs in clusters.items():
-        result[person] = [f"http://127.0.0.1:8000/images/{os.path.basename(p)}" for p in imgs]
+        result[person] = [
+            f"http://{os.getenv('PUBLIC_IP', '13.239.35.7')}:8000/images/{os.path.basename(p)}"
+            for p in imgs
+        ]
 
     return JSONResponse({"clusters": result})
